cce1179624ba1e49e5a1cbfc395c1da2c4ab2462
Tabish <taabishm2@gmail.com>
2019-07-08 23:18:10 +0530

    reset server command

--- responselistener.py
+++ responselistener.py
@@ -1,22 +1,27 @@
 import time
 import fetchcommand
+import requests
 
 def listen():
     for lim in range(100):  #Try for 5 minutes to get a new command
         try:
             f = fetchcommand.fetchcmd()
         except:
-            raise("fetchcommand Exception: Couldn't fetch from server")
+            raise Exception("fetchcommand Exception: Couldn't fetch from server")
 
         if f == '420':      #Implies no new command, wait for 5 seconds
+            print("Waiting")
             time.sleep(5)
         elif f.split()[0] == '421':     #Implies faulty handling on server
-            raise("421 Error: Server code fault")
+            raise Exception("421 Error: Server code fault")
+            requests.get("http://taabishm2.pythonanywhere.com/resetcmd")
             return
         else:
+            requests.get("http://taabishm2.pythonanywhere.com/resetcmd")
             return f.lower()
     else:
-        raise("Listener timed out.")
+        requests.get("http://taabishm2.pythonanywhere.com/resetcmd")
+        raise Exception("Listener timed out.")
 
 if __name__ == '__main__':
     print(listen())
